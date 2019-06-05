be06e67fa7de763f28f9f6926473284cf319517c
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-06-05 12:34:57 +0300

    Use async handlers

--- koshkaas/main.py
+++ koshkaas/main.py
@@ -2,6 +2,18 @@ import kopf
 
 
 @kopf.on.create('lcm.miratis.com', 'v1alpha1', 'openstackdeployments')
-def deploy(body, spec, **kwargs):
+async def create_fn(body, spec, **kwargs):
     print(f"And here we are! Creating: {spec}")
     return {'message': 'hello world'}  # will be the new status
+
+
+@kopf.on.update('lcm.miratis.com', 'v1alpha1', 'openstackdeployments')
+async def update_fn(body, spec, **kwargs):
+    print(f"And here we are! Creating: {spec}")
+    return {'message': 'new world'}  # will be the new status
+
+
+@kopf.on.delete_fn('lcm.miratis.com', 'v1alpha1', 'openstackdeployments')
+async def delete(body, spec, **kwargs):
+    print(f"And here we are! Creating: {spec}")
+    return {'message': 'by world'}  # will be the new status
