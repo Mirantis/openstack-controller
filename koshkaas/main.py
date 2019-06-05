6ba397b23bc04a9385dfacf19c470aa1232424cd
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-06-05 12:44:22 +0300

    Fix typo in controller :facepalm:

--- koshkaas/main.py
+++ koshkaas/main.py
@@ -1,19 +1,19 @@
 import kopf
 
 
-@kopf.on.create('lcm.miratis.com', 'v1alpha1', 'openstackdeployments')
+@kopf.on.create('lcm.mirantis.com', 'v1alpha1', 'openstackdeployments')
 async def create_fn(body, spec, **kwargs):
     print(f"And here we are! Creating: {spec}")
     return {'message': 'hello world'}  # will be the new status
 
 
-@kopf.on.update('lcm.miratis.com', 'v1alpha1', 'openstackdeployments')
+@kopf.on.update('lcm.mirantis.com', 'v1alpha1', 'openstackdeployments')
 async def update_fn(body, spec, **kwargs):
     print(f"And here we are! Creating: {spec}")
     return {'message': 'new world'}  # will be the new status
 
 
-@kopf.on.delete_fn('lcm.miratis.com', 'v1alpha1', 'openstackdeployments')
+@kopf.on.delete_fn('lcm.mirantis.com', 'v1alpha1', 'openstackdeployments')
 async def delete(body, spec, **kwargs):
     print(f"And here we are! Creating: {spec}")
     return {'message': 'by world'}  # will be the new status
