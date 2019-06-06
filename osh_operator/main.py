8b2d69041401a6e87d2c55580204aedd496fc348
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-06-06 20:57:39 +0300

    Add initial templating support
    using Jinja2, only template stubs for infra services are added
    (ingress, mariadb, memcached, rabbitmq).
    
    Change-Id: Ia307398040a2ecfe11b7ee3de80ca17aa823b9f6
    Related-Issue: PROD-30884

--- osh_operator/main.py
+++ osh_operator/main.py
@@ -1,33 +1,41 @@
+import functools
+
+import jinja2
 import kopf
 import kubernetes
-import os
-import pathlib
 import yaml
 
 
-@kopf.on.create("lcm.mirantis.com", "v1alpha1", "openstackdeployments")
-async def create(body, spec, logger, **kwargs):
-    root_templates = "/opt/osh_operator"
-    if os.getenv("OSH_OPERATOR_DEV", None):
-        root_templates = __file__
-    p = (
-        pathlib.Path(root_templates).parent
-        / "templates"
-        / "stein"
-        / "ingress.yaml"
-    )
-    with p.open() as f:
-        data = yaml.safe_load(f)
-    data["spec"]["repositories"] = spec["common"]["charts"]["repositories"]
+ENV = jinja2.Environment(loader=jinja2.PackageLoader("osh_operator"))
 
+
+def handle_service(service, *, body, meta, spec, logger, **kwargs):
+    logger.info(f"found templates {ENV.list_templates()}")
+    os_release = spec["common"]["openstack"]["version"]
+    tpl = ENV.get_template(f"{os_release}/{service}.yaml")
+    logger.info(f"template file is {tpl.filename}")
+    text = tpl.render(body=body, meta=meta, spec=spec)
+    data = yaml.safe_load(text)
+    # FIXME(pas-ha) either move to dict merging stage before,
+    # or move to the templates themselves
+    data["spec"]["repositories"] = spec["common"]["charts"]["repositories"]
     kopf.adopt(data, body)
 
     api = kubernetes.client.CustomObjectsApi()
     obj = api.create_namespaced_custom_object(
         "lcm.mirantis.com", "v1alpha1", "default", "helmbundles", body=data
     )
-
     logger.info(f"HelmBundle child is created: %s", obj)
+
+
+@kopf.on.create("lcm.mirantis.com", "v1alpha1", "openstackdeployments")
+async def create(body, meta, spec, logger, **kwargs):
+    service_fns = {}
+    for service in spec.get("services", {}).keys():
+        service_fns[service] = functools.partial(
+            handle_service, service=service
+        )
+    await kopf.execute(fns=service_fns)
     return {"message": "created!"}
 
 
@@ -35,7 +43,7 @@ async def create(body, spec, logger, **kwargs):
 async def update(body, spec, logger, diff, patch, **kwargs):
     logger.info(f"Update DIFF is {diff}")
     logger.info(f"Update PATCH is {patch}")
-    return {"message": "update called"}
+    return {"message": "updated {meta['name']} with {diff}"}
 
 
 @kopf.on.delete("lcm.mirantis.com", "v1alpha1", "openstackdeployments")
