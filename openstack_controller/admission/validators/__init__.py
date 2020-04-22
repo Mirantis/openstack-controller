71efe815d65f53a97a0682274653cdd2795d8592
Vladyslav Drok <vdrok@mirantis.com>
2020-04-22 16:00:54 +0200

    Remove barbican validator
    As of now there is nothing required in the barbican features section
    in the osdpl object, so just remove it.
    
    Related-issue: https://mirantis.jira.com/browse/PRODX-848
    Change-Id: I79aa3548df0913a4f2c828f606ba314e88ebe8e4

--- openstack_controller/admission/validators/__init__.py
+++ openstack_controller/admission/validators/__init__.py
@@ -1,7 +1,5 @@
-from openstack_controller.admission.validators import barbican
 from openstack_controller.admission.validators import keystone
 
 __all__ = [
-    barbican.BarbicanValidator,
     keystone.KeystoneValidator,
 ]
