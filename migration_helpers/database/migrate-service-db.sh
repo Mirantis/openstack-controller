1a1c85a547a415be065d974e6bfc1cfc138a68fe
Vasyl Saienko <vsaienko@mirantis.com>
2020-08-21 13:24:00 +0000

    [migration_helpers] database improvements
     * Add more logging
     * safe check non existed DBs
    
    Related-Prod: PRODX-6790
    Change-Id: If76e3a8faf995dc6ae5c8ac91971850062f2e683

--- migration_helpers/database/migrate-service-db.sh
+++ migration_helpers/database/migrate-service-db.sh
@@ -7,10 +7,16 @@ TOP_DIR=$( cd $(dirname $RUN_DIR/../../) && pwd)
 
 mkdir -p $DATABASE_DIR
 for component in $COMPONENTS_TO_MIGRATE_DB; do
+    info "Calling check_database_connection"
     check_database_connection $component $(get_mcp1_database_address) $(get_mcp1_database_username $component) $(get_mcp1_database_password $component)
+    info "Calling get_database_size"
     get_database_size $component $(get_mcp1_database_address) $(get_mcp1_database_username $component) $(get_mcp1_database_password $component)
+    info "Calling dump_openstack_component_dbs"
     dump_openstack_component_dbs $component
+    info "Calling check_database_connection"
     check_database_connection $component $(get_mcp2_database_address) $(get_mcp2_database_username $component) $(get_mcp2_database_password $component)
+    info "Calling drop_database_on_target"
     drop_database_on_target $component
+    info "Calling import_openstack_component_dbs"
     import_openstack_component_dbs $component
 done
