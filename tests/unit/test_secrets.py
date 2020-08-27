8a924730c9bc9a596d714149386bb38743ba63ee
Ivan Kolodyazhny <e0ne@e0ne.info>
2020-08-27 18:23:53 +0300

    Fix KeycloakSecret decoding
    Change-Id: Ib6ffb520871a457ecf202752c87c78c9247ab258
    Closes: PROD-6633

--- tests/unit/test_secrets.py
+++ tests/unit/test_secrets.py
@@ -1,4 +1,8 @@
+import base64
+import json
 from unittest import mock
+
+import pykube
 
 from openstack_controller import secrets
 
@@ -22,3 +26,26 @@ def test_openstack_admin_secret_create_password(mock_password):
     assert creds.messaging.password == password
 
     assert mock_password.call_count == 3
+
+
+@mock.patch("openstack_controller.secrets.get_secret_data")
+@mock.patch("openstack_controller.secrets.generate_password")
+def test_keycloak_secret_serialization(mock_password, mock_data):
+    passphrase = "passphrase"
+    mock_password.return_value = passphrase
+
+    secret_data = {
+        "passphrase": base64.b64encode(json.dumps(passphrase).encode())
+    }
+
+    mock_data.side_effect = [pykube.exceptions.ObjectDoesNotExist, secret_data]
+
+    secret = secrets.KeycloakSecret("ns")
+
+    # NOTE(e0ne): ensure will create a secret if it's not found in K8S, so the
+    # second call should just read the secret from the K8S.
+    created = secret.ensure().passphrase
+    from_secret = secret.ensure().passphrase
+
+    assert created == passphrase
+    assert created == from_secret
