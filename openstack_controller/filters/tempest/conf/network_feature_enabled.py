a073922ed95fb17cb1f3d79da8a259c255cb2175
Ilya Bumarskov <bumarskov@gmail.com>
2021-12-30 17:31:52 +0400

    Enable missing networks extensions in case of TF
    Related-PROD: PRODX-18155, PRODX-20775, PRODX-20776
    Change-Id: I09c28f221db11f72da91e60ba2348cc2f288873c

--- openstack_controller/filters/tempest/conf/network_feature_enabled.py
+++ openstack_controller/filters/tempest/conf/network_feature_enabled.py
@@ -77,6 +77,11 @@ class NetworkFeatureEnabled(base_section.BaseSection):
                 >= constants.OpenStackVersion["ussuri"]
             ):
                 api_extensions_default.append("rbac-address-scope")
+            if (
+                self.get_spec_item("features.neutron.backend")
+                == "tungstenfabric"
+            ):
+                api_extensions_default.extend(["standard-attr-tag", "trunk"])
 
         if self.get_spec_item("features.neutron.bgpvpn.enabled"):
             api_extensions_default.extend(
