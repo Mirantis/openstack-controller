330f61cab8f98584756bdb8cdbfea8c38f314fde
Vasyl Saienko <vsaienko@mirantis.com>
2022-01-26 13:12:34 +0000

    Ensure default kopf finalizers removed
    The patch adds init container to remove defalt kopf finalizer from
    objects if present.
    
    Related-Prod: PRODX-21317
    
    Change-Id: I50c90258480893e23363e12e46b53e035e100683

---
+++ openstack_controller/cli/remove_default_finalizer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import asyncio
+
+import pykube
+
+from openstack_controller import kube
+from openstack_controller import settings
+from openstack_controller import utils
+
+
+LOG = utils.get_logger("remove-old-finalizer")
+
+DEFAULT_KOPF_FINALIZER = "kopf.zalando.org/KopfFinalizerMarker"
+
+
+async def _main():
+    for kclass in kube.KUBE_OBJECTS.values():
+        namespace = settings.OSCTL_OS_DEPLOYMENT_NAMESPACE
+        if not issubclass(kclass, pykube.objects.NamespacedAPIObject):
+            namespace = None
+        for obj in kube.resource_list(
+            kclass, selector=None, namespace=namespace
+        ):
+            if DEFAULT_KOPF_FINALIZER in obj.metadata.get("finalizers", []):
+                LOG.info(
+                    f"Removing default finalizer for {kclass.__name__}: {obj.metadata['name']}"
+                )
+                finalizers = obj.metadata["finalizers"]
+                finalizers.remove(DEFAULT_KOPF_FINALIZER)
+                obj.patch({"metadata": {"finalizers": finalizers}})
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(_main())
