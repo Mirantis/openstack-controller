a6621353af38ae72540f3fb9b15b723aeadaee12
Alexander Noskov <anoskov@mirantis.com>
2022-12-07 15:11:29 -0600

    Fix keystone fernet sync in 02-sync-fernet-keys.sh migration script
    * Problem description:
    In MOSK keystone pod using secret with names:
      - keystone-fernet-data
      - keystone-credential-data
    But 02-sync-fernet-keys.sh script creates:
      - keystone-fernet-keys
      - keystone-credential-keys
    
    * Also I found this script not optimal and refactored it to use
    salt cp.push_dir to push directory with content from minion to
    master which also reduced the time.
    
    * Additionally added "file_recv: True" parameter for salt-master
    config file to be able receive files from minions.
    
    Change-Id: I02dc5f94caf79e94d5a98ce0e18588129eeb6725

--- migration_helpers/keystone/02-sync-fernet-keys.sh
+++ migration_helpers/keystone/02-sync-fernet-keys.sh
@@ -8,55 +8,20 @@ TOP_DIR=$(cd $(dirname $RUN_DIR/../../) && pwd)
 . $TOP_DIR/globals
 . $TOP_DIR/functions-common
 
-function get_key_archive_path {
-  local key_dir=$1
-  echo "${key_dir}.tar.gz"
-}
-
-function pack_keystone_keys {
-    local salt_out
-
-    info "Packing keystone repository ${KEY_REPOSITORY}"
-    for key_dir in ${KEY_REPOSITORY}; do
-        info "Packing keystone key repository ${key_dir}"
-        key_directory=$(basename ${key_dir})
-        repository_path=$(dirname ${key_dir})
-        key_arc_name="${key_directory}.tar.gz"
-        salt_out=$(salt 'ctl01*' archive.tar czf $(get_key_archive_path $key_dir) ./${key_directory} cwd=\"${repository_path}\") || die $LINENO "Failed to pack keystone key $key_dir"
-        info "Keystone keys were packed"
-    done
-}
-
-function migrate_keystone_keys {
-    local keys_pkg=$1
-    local minion_ID
-    local local_keys_pkg
-    local local_pkg_name
-    local keys_pkg_dirname
-    local key_type=$(basename ${keys_pkg} .tar.gz)
-
-    info "Migrating keystone key: $keys_pkg"
-    local_pkg_name=$(basename ${keys_pkg})
-    minion_ID=$(salt 'ctl01*' cp.push ${keys_pkg} upload_path=\"/${local_pkg_name}\" remove_source=True --out=json | jq 'keys[]' | tr -d '"')
-    local_keys_pkg="/var/cache/salt/master/minions/${minion_ID}/files/${local_pkg_name}"
-
-    keys_pkg_dirname=$(mktemp -d)
-    tar -xzf ${local_keys_pkg} -C ${keys_pkg_dirname}/ || die $LINENO "Failed to extract archive with keys: $local_keys_pkg"
-    kubectl delete secret keystone-${key_type} -n openstack || die $LINENO "Failed to delete secret with keys: keystone-${key_type}"
-    kubectl create secret generic keystone-${key_type} --from-file=${keys_pkg_dirname}/${key_type} -n openstack || die $LINENO "Failed to update keys: keystone-${key_type}"
-    rm -rf $keys_pkg_dirname
-    info "Keys were updated: $keys_pkg"
-}
+MASTER_CP_NODE=$(salt -C 'I@keystone:server:role:primary' grains.get fqdn --out newline_values_only)
+CACHE_DIR="/var/cache/salt/master/minions/${MASTER_CP_NODE}/files/var/lib/keystone"
+for key_dir in ${KEY_REPOSITORY}; do
+    salt $MASTER_CP_NODE cp.push_dir $key_dir
+    key_type=$(basename ${key_dir} | cut -d- -f1)
+    mv ${CACHE_DIR}/${key_type}-{keys,data}
+    kubectl delete secret keystone-${key_type}-data -n openstack
+    kubectl create secret generic keystone-${key_type}-data --from-file=${CACHE_DIR}/${key_type}-data -n openstack || die $LINENO "Failed to update keys: keystone-${key_type}"
+done
 
 function restart_keystone_api {
     info "Restarting keystone-api on MCP2"
     kubectl delete pods -l application=keystone,component=api -n openstack || die $LINENO "Failed to restart keystone api"
     info "Keystone API was restarted"
 }
-
-pack_keystone_keys
-for key_dir in ${KEY_REPOSITORY}; do
-    migrate_keystone_keys $(get_key_archive_path $key_dir)
-done
 
 restart_keystone_api
