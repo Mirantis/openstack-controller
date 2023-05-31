4c3e4c32e33ab3e34497291e989b6ddcbbcfa6c5
Vasyl Saienko <vsaienko@mirantis.com>
2023-05-31 10:26:44 +0000

    Optimize run_nova_cell_setup function
    Switch to  cronjob.run functionality to rerun cronjob.
    
    Related-Prod: PRODX-3456
    Change-Id: I227f52fe7816191139eac0d9a1e0e4816a4e47c7

--- openstack_controller/hooks.py
+++ openstack_controller/hooks.py
@@ -1,6 +1,3 @@
-import asyncio
-
-import kopf
 import pykube
 
 from openstack_controller import kube
@@ -24,32 +21,9 @@ async def run_nova_cell_setup(osdpl, name, namespace, meta, **kwargs):
     LOG.info("Start nova daemonset created hook")
     if not new_node_added(**kwargs):
         return
-    cronjob = kube.find(pykube.CronJob, "nova-cell-setup", namespace)
-    job = {
-        "metadata": {
-            "name": "nova-cell-setup-online",
-            "namespace": namespace,
-            "annotations": cronjob.obj["metadata"]["annotations"],
-            "labels": cronjob.obj["spec"]["jobTemplate"]["metadata"]["labels"],
-        },
-        "spec": cronjob.obj["spec"]["jobTemplate"]["spec"],
-    }
-    job["spec"]["backoffLimit"] = 10
-    job["spec"]["ttlSecondsAfterFinished"] = 60
-    job["spec"]["template"]["spec"]["restartPolicy"] = "OnFailure"
-    kopf.adopt(job, osdpl.obj)
-    kube_job = kube.Job(kube.api, job)
-    try:
-        kube_job.create()
-    except pykube.exceptions.HTTPError as e:
-        LOG.info(f"Cannot create nova-cell-setup-job, error: {e}")
-        job_already_exists = e.code == 409
-        if not job_already_exists:
-            raise
-        if kube_job.ready:
-            kube_job.delete()
-            await asyncio.sleep(1)
-            await run_nova_cell_setup(osdpl, name, namespace, meta, **kwargs)
+    cronjob = kube.find(kube.CronJob, "nova-cell-setup", namespace)
+    job = await cronjob.run(wait_completion=True)
+    job.delete()
 
 
 async def run_octavia_create_resources(osdpl, name, namespace, meta, **kwargs):
