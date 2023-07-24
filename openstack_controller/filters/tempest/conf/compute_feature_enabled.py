34d343085d77eb5bacaf0626081a9981a32078e9
dbiletskiy <dbiletskiy@mirantis.com>
2023-07-24 12:42:23 +0200

    Add antelope support initial
    * allow skip-level upgrade for SLURP releases
    * horizon - add dashboard path for 3.10 python version
    
    Related-Prod: PRODX-33836 PRODX-34549
    Change-Id: Ibe23dba4c15a37600c7c172ec851ad6cdb4d2140

--- openstack_controller/filters/tempest/conf/compute_feature_enabled.py
+++ openstack_controller/filters/tempest/conf/compute_feature_enabled.py
@@ -2,6 +2,7 @@ from openstack_controller.filters.tempest import base_section
 
 
 MULTIATTACH_CEPH_RELEASE_MAPPING = {
+    "antelope": True,
     "yoga": True,
     "xena": True,
     "wallaby": True,
