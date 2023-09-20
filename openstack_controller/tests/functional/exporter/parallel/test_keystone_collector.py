dce356a91da1b25b39de18df0fb27b7cb844d47c
Vasyl Saienko <vsaienko@mirantis.com>
2023-09-20 10:46:31 +0000

    Add keystone metrics tests
    Related-Prod: PRODX-3456
    Change-Id: I320ef5fa91054da88269536eaf9302a975a75316

---
+++ openstack_controller/tests/functional/exporter/parallel/test_keystone_collector.py
@@ -0,0 +1,32 @@
+from openstack_controller.tests.functional.exporter import base
+
+
+class KeystoneCollectorFunctionalTestCase(base.BaseFunctionalExporterTestCase):
+    known_metrics = {
+        "osdpl_keystone_projects": {"labels": []},
+        "osdpl_keystone_users": {"labels": []},
+    }
+
+    def test_keystone_projects_value(self):
+        projects_total = 0
+        for domain in self.ocm.oc.identity.domains():
+            projects_total += len(
+                list(self.ocm.oc.identity.projects(domain_id=domain["id"]))
+            )
+        metric = self.get_metric("osdpl_keystone_projects")
+        self.assertEqual(
+            projects_total,
+            metric.samples[0].value,
+        )
+
+    def test_keystone_users_value(self):
+        users_total = 0
+        for domain in self.ocm.oc.identity.domains():
+            users_total += len(
+                list(self.ocm.oc.identity.users(domain_id=domain["id"]))
+            )
+        metric = self.get_metric("osdpl_keystone_users")
+        self.assertEqual(
+            users_total,
+            metric.samples[0].value,
+        )
