f32526c55101d0207af792585a2b42c5ee99768b
Vasyl Saienko <vsaienko@mirantis.com>
2023-10-05 12:58:29 +0000

    [exporter] Reinitialize openstack client when update_samples failed
    If we failed with some exception in update_samples() this may be caused
    by stale catalog in openstack client. Reiinitialize it in this case.
    
    Related-Prod: PRODX-35904
    Change-Id: I7c4cf65127669b130a25dd0a8512cfff29d385b5

--- openstack_controller/exporter/collectors/openstack/base.py
+++ openstack_controller/exporter/collectors/openstack/base.py
@@ -31,7 +31,9 @@ class OpenStackBaseMetricCollector(base.BaseMetricsCollector):
 
     @property
     def oc(self):
-        if self._oc is None:
+        # NOTE(vsaienko): if we got any exception during  update_samples
+        # re initialize oc in this case.
+        if not self.scrape_success or self._oc is None:
             try:
                 self._oc = openstack_utils.OpenStackClientManager()
             except Exception as e:
@@ -42,26 +44,13 @@ class OpenStackBaseMetricCollector(base.BaseMetricsCollector):
     @property
     def is_service_available(self):
         services = []
-        cached_endpoints = []
         for service_type in self._os_service_types:
-            # NOTE(vsaienko): endpoint_for returns list of endpoints from cache
-            # during client initialization. identity.services returns actual
-            # services from keystone catalog. If they do not mutch, service added
-            # or removed. Reinitialize client to make sure we use updated catalog.
-            cached_endpoints.append(self.oc.oc.endpoint_for(service_type))
             services.append(
                 len(list(self.oc.oc.identity.services(type=service_type)))
             )
         if not any(services):
             LOG.info(f"Service not found for types {self._os_service_types}")
             return False
-        else:
-            if not any(cached_endpoints):
-                LOG.info(
-                    f"Catalog is changed for service types: {self._os_service_types}"
-                )
-                self._oc = None
-                return False
         return True
 
     @property
