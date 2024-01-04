9f59fdcd19698af784e6fae2a53b4761afee85df
Aleksandr Pashkovskyi <opashkovskyi@mirantis.com>
2024-01-04 09:36:29 +0200

    Add functional tests for osdpl_keystone_domains metric
    Related-prod: PRODX-37792
    Change-Id: I4ee2b13258ad1442934ae3b1c90fbecc078eea20

--- openstack_controller/tests/functional/exporter/parallel/test_keystone_collector.py
+++ openstack_controller/tests/functional/exporter/parallel/test_keystone_collector.py
@@ -5,6 +5,7 @@ class KeystoneCollectorFunctionalTestCase(base.BaseFunctionalExporterTestCase):
     known_metrics = {
         "osdpl_keystone_projects": {"labels": []},
         "osdpl_keystone_users": {"labels": []},
+        "osdpl_keystone_domains": {"labels": []},
     }
 
     def test_keystone_projects_value(self):
@@ -28,5 +29,13 @@ class KeystoneCollectorFunctionalTestCase(base.BaseFunctionalExporterTestCase):
         metric = self.get_metric("osdpl_keystone_users")
         self.assertEqual(
             users_total,
+            metric.samples[0].value,
+        )
+
+    def test_keystone_domains(self):
+        domains_total = len(list(self.ocm.oc.identity.domains()))
+        metric = self.get_metric("osdpl_keystone_domains")
+        self.assertEqual(
+            domains_total,
             metric.samples[0].value,
         )
