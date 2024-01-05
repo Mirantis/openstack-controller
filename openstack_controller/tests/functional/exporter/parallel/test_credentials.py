27ca3fe26a11cbd45f05429f2e2b82dc41b0cba4
Mykyta Karpin <mkarpin@mirantis.com>
2024-01-05 15:46:53 +0200

    [exporter][tests] Add checks for rotation timestamps values
    Related-Prod: https://mirantis.jira.com/browse/PRODX-38401
    Change-Id: Id0cfa4c43bdf744818f88c76462a11a9d729bbaa

--- openstack_controller/tests/functional/exporter/parallel/test_credentials.py
+++ openstack_controller/tests/functional/exporter/parallel/test_credentials.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from openstack_controller.tests.functional.exporter import base
+from openstack_controller.osdplstatus import OpenStackDeploymentStatus
+from parameterized import parameterized
 
 
 class CredentialsCollectorFunctionalTestCase(
@@ -7,12 +10,27 @@ class CredentialsCollectorFunctionalTestCase(
     def setUp(self):
         super().setUp()
         self.metric = self.get_metric("osdpl_credentials_rotation_timestamp")
+        self.osdplst = OpenStackDeploymentStatus(
+            self.osdpl.name, self.osdpl.namespace
+        )
 
     def test_metric_present(self):
         self.assertIsNotNone(self.metric)
 
-    def test_rotation_samples(self):
-        for _type in ["admin", "service"]:
-            labels = {"type": _type}
-            samples = self.filter_metric_samples(self.metric, labels)
-            self.assertEqual(1, len(samples))
+    @parameterized.expand(
+        [
+            ("admin"),
+            ("service"),
+        ]
+    )
+    def test_rotation_samples(self, creds_type):
+        labels = {"type": creds_type}
+        samples = self.filter_metric_samples(self.metric, labels)
+        self.assertEqual(1, len(samples))
+        status_ts = self.osdplst.get_credentials_rotation_status(creds_type)[
+            "timestamp"
+        ]
+        status_unix_ts = datetime.strptime(
+            status_ts, "%Y-%m-%d %H:%M:%S.%f"
+        ).timestamp()
+        self.assertEqual(status_unix_ts, samples[0].value)
