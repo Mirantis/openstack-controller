d5edea85e8cec9c7a78ac3f7727f4ac97c9f8dd6
Aleksandr Pashkovskyi <opashkovskyi@mirantis.com>
2024-01-17 10:47:52 +0200

    Add test_keystone_domains for test_keystone_collector
    Add for test_keystone_domains create and delete parts
    Also, add create_domain and delete_domain functions in functional/base.py
    
    Related-prod: PRODX-38667
    Change-Id: I4846ff19c32b65175b9598fa701852452ae2d686

--- openstack_controller/tests/functional/exporter/parallel/test_keystone_collector.py
+++ openstack_controller/tests/functional/exporter/parallel/test_keystone_collector.py
@@ -1,4 +1,5 @@
 from openstack_controller.tests.functional.exporter import base
+from openstack_controller.tests.functional import data_utils
 
 
 class KeystoneCollectorFunctionalTestCase(base.BaseFunctionalExporterTestCase):
@@ -7,6 +8,22 @@ class KeystoneCollectorFunctionalTestCase(base.BaseFunctionalExporterTestCase):
         "osdpl_keystone_users": {"labels": []},
         "osdpl_keystone_domains": {"labels": []},
     }
+
+    scrape_collector = "osdpl_keystone"
+
+    def setUp(self):
+        super().setUp()
+        self.domain_name = data_utils.rand_name()
+
+    def _test_osdpl_keystone_domains(self, metric_name, expected_num, phase):
+        metric = self.get_metric_after_refresh(
+            metric_name, self.scrape_collector
+        )
+        self.assertEqual(
+            metric.samples[0].value,
+            expected_num,
+            f"{phase}: The number of domains is not correct.",
+        )
 
     def test_keystone_projects_value(self):
         projects_total = 0
@@ -33,9 +50,25 @@ class KeystoneCollectorFunctionalTestCase(base.BaseFunctionalExporterTestCase):
         )
 
     def test_keystone_domains(self):
+        metric_name = "osdpl_keystone_domains"
         domains_total = len(list(self.ocm.oc.identity.domains()))
-        metric = self.get_metric("osdpl_keystone_domains")
-        self.assertEqual(
-            domains_total,
-            metric.samples[0].value,
+
+        self._test_osdpl_keystone_domains(
+            metric_name, domains_total, "Initial"
+        )
+
+        # Add one domain
+        domain = self.create_domain(self.domain_name)
+
+        # Refresh and check new domain is in list:
+        self._test_osdpl_keystone_domains(
+            metric_name, domains_total + 1, "After create"
+        )
+
+        # Delete domain:
+        self.delete_domain(domain["id"])
+
+        # Check that domain had been deleted from list:
+        self._test_osdpl_keystone_domains(
+            metric_name, domains_total, "After delete"
         )
