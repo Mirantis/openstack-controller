f38f2515b48aa4a1e5b9ff73ee2f408247639745
dbiletskiy <dbiletskiy@mirantis.com>
2024-03-12 20:02:30 +0100

    Add test scheme for check librbd and libvirt/qemu-utils packages
    Related-Prod: PRODX-35701
    Change-Id: I94ef3cef354b8b4691a152fc023fd132d3c0374b

--- openstack_controller/tests/functional/deployment/parallel/test_component_versions.py
+++ openstack_controller/tests/functional/deployment/parallel/test_component_versions.py
@@ -1,3 +1,5 @@
+import re
+
 from parameterized import parameterized
 from openstack_controller.tests.functional import base
 from openstack_controller import settings
@@ -13,81 +15,124 @@ class ComponentVersionsFunctionalTestCase(base.BaseFunctionalTestCase):
                     "pkg_names": ["ceph-common"],
                     "pkg_type": "dpkg",
                     "check_pods": [
-                        {
-                            "application": "libvirt",
-                            "component": "libvirt",
-                            "container": "libvirt",
-                        },
-                        {
-                            "application": "cinder",
-                            "component": "volume",
-                            "container": "cinder-volume",
-                        },
-                        {
-                            "application": "glance",
-                            "component": "api",
-                            "container": "glance-api",
-                        },
-                        {
-                            "application": "nova",
-                            "component": "compute",
-                            "container": "nova-compute",
-                        },
-                        {
-                            "application": "manila",
-                            "component": "api",
-                            "container": "manila-api",
-                        },
+                        "libvirt",
+                        "cinder_volume",
+                        "glance_api",
+                        "nova_compute",
+                        "manila_api",
                     ],
-                }
+                },
+            ],
+            "libvirt": [
+                {
+                    "pkg_names": ["libvirt0:amd64", "qemu-utils"],
+                    "pkg_type": "dpkg",
+                    "check_pods": [
+                        "libvirt",
+                        "nova_compute",
+                    ],
+                },
+            ],
+            "librbd": [
+                {
+                    "pkg_names": ["librbd1", "python3-rbd"],
+                    "pkg_type": "dpkg",
+                    "check_pods": [
+                        "libvirt",
+                        "cinder_volume",
+                        "glance_api",
+                        "nova_compute",
+                    ],
+                },
             ],
         }
 
-    def _get_command_line(self, pkg_type, pkg_name):
+        self.pods_scheme = {
+            "libvirt": {
+                "application": "libvirt",
+                "component": "libvirt",
+                "container": "libvirt",
+            },
+            "cinder_volume": {
+                "application": "cinder",
+                "component": "volume",
+                "container": "cinder-volume",
+            },
+            "glance_api": {
+                "application": "glance",
+                "component": "api",
+                "container": "glance-api",
+            },
+            "nova_compute": {
+                "application": "nova",
+                "component": "compute",
+                "container": "nova-compute",
+            },
+            "manila_api": {
+                "application": "manila",
+                "component": "api",
+                "container": "manila-api",
+            },
+        }
+
+    def _get_pkg_version(self, pkg_type, pkg_name, pod, container):
         if pkg_type == "pip":
             command = ["pip", "show", pkg_name]
         elif pkg_type == "dpkg":
             command = ["dpkg", "-s", pkg_name]
         else:
             assert False, f"Unsupported version type: {pkg_type}"
-        return command
+        pod_stdout = pod.exec(command, container=container)["stdout"]
+        match = re.search(r"^Version:\s*(.+)$", pod_stdout, re.MULTILINE)
+        self.assertTrue(
+            match,
+            f"Failed to get version of {pkg_name} in pod: {pod}",
+        )
+        pkg_version = match.group(1).strip()
+        return pkg_version
+
+    def _get_pod_scheme(self, check_pod):
+        return self.pods_scheme[check_pod]
 
     @parameterized.expand(
         [
             ("ceph"),
+            ("libvirt"),
+            ("librbd"),
         ]
     )
-    def test_check_version_are_same(self, scheme_name):
+    def test_check_package_version_are_same(self, scheme_name):
         kube_api = kube.kube_client()
-        have_different_version = {}
-        scheme = self.check_scheme[scheme_name][0]
-        for pkg_name in scheme["pkg_names"]:
-            versions = {}
-            command = self._get_command_line(scheme["pkg_type"], pkg_name)
-            for check_pod in scheme["check_pods"]:
-                pods = kube.Pod.objects(kube_api).filter(
-                    namespace=settings.OSCTL_OS_DEPLOYMENT_NAMESPACE,
-                    selector={
-                        "application": check_pod["application"],
-                        "component": check_pod["component"],
-                    },
-                )
-                pods = [pod for pod in pods]
-                if not pods:
-                    continue
-                pod = pods[0]
-                version = pod.exec(command, container=check_pod["container"])[
-                    "stdout"
-                ]
+        have_different_pkg_version = {}
+        for scheme in self.check_scheme[scheme_name]:
+            for pkg_name in scheme["pkg_names"]:
+                pkg_versions = {}
+                for check_pod in scheme["check_pods"]:
+                    pod_scheme = self._get_pod_scheme(check_pod)
+                    pods = kube.Pod.objects(kube_api).filter(
+                        namespace=settings.OSCTL_OS_DEPLOYMENT_NAMESPACE,
+                        selector={
+                            "application": pod_scheme["application"],
+                            "component": pod_scheme["component"],
+                        },
+                    )
+                    pods = [pod for pod in pods]
+                    if not pods:
+                        continue
+                    pod = pods[0]
+                    pkg_versions[pod] = self._get_pkg_version(
+                        scheme["pkg_type"],
+                        pkg_name,
+                        pod,
+                        pod_scheme["container"],
+                    )
                 self.assertTrue(
-                    version
-                ), f"Failed to get version of {pkg_name} in pod: {check_pod}"
-                versions[pod] = version
-            self.assertTrue(
-                versions
-            ), f"Failed to get version of {pkg_name} in pods: {scheme['check_pods']}"
-            if len(set(versions.values())) != 1:
-                have_different_version[pkg_name] = versions
+                    pkg_versions,
+                    f"Failed to get version of {pkg_name} in pods: {scheme['check_pods']}",
+                )
+                if len(set(pkg_versions.values())) != 1:
+                    have_different_pkg_version[pkg_name] = pkg_versions
         self.assertFalse(
-            have_different_version
-        ), f"Packages have different version: {have_different_version}"
+            have_different_pkg_version,
+            f"Packages have different version: {have_different_pkg_version}",
+        )
