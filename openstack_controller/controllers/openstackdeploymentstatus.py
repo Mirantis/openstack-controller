390972db424b048daf41a9434908b8d5828f208b
Vasyl Saienko <vsaienko@mirantis.com>
2024-04-17 08:52:13 +0000

    [synchronize] Make osdplstatus controller sync
    We have sync calls like kopf and openstacksdk so switch to sync mode
    to let kopf running tasks in threads and do not block main thread.
    
    Related-Prod: PRODX-41810
    Change-Id: If197a466d9714225bd7c1bbc863f2dfae68ff25c
    (cherry picked from commit e403c034666a42342ddfc00efaa487ec281eaa0a)

--- openstack_controller/controllers/openstackdeploymentstatus.py
+++ openstack_controller/controllers/openstackdeploymentstatus.py
@@ -15,7 +15,7 @@ LOG = utils.get_logger(__name__)
 @kopf.on.resume(*osdplstatus.OpenStackDeploymentStatus.kopf_on_args)
 @kopf.on.update(*osdplstatus.OpenStackDeploymentStatus.kopf_on_args)
 @kopf.on.create(*osdplstatus.OpenStackDeploymentStatus.kopf_on_args)
-async def handle(body, meta, spec, logger, reason, **kwargs):
+def handle(body, meta, spec, logger, reason, **kwargs):
     # TODO(pas-ha) remove all this kwargs[*] nonsense, accept explicit args,
     # pass further only those that are really needed
     # actual **kwargs form is for forward-compat with kopf itself
@@ -29,7 +29,7 @@ async def handle(body, meta, spec, logger, reason, **kwargs):
     field="status.services",
 )
 @kopf.on.resume(*osdplstatus.OpenStackDeploymentStatus.kopf_on_args)
-async def osdplst_status_services(
+def osdplst_status_services(
     name, namespace, body, meta, spec, logger, reason, **kwargs
 ):
     LOG.info(f"Got osdplstatus status.services event {reason}")
